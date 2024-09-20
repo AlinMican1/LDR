90219ad8cf42b88104691663af4d6a5b7eac739b
AlinMican <AlinMican2001@gmail.com>
2024-09-20 12:52:24 +0100

    first commit

---
+++ src/app/api/users/route.tsx
@@ -0,0 +1,10 @@
+import connectToDB from "../../../../lib/database";
+import User from "../../../../models/user";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { name, age } = await request.json();
+  await connectToDB();
+  await User.create({ name, age });
+  return NextResponse.json({ message: "User Created" }, { status: 201 });
+}
