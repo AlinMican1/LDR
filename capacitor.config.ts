06543d61d1c94058215dba93d947939ae51da7ac
AlinMican <AlinMican2001@gmail.com>
2024-09-23 14:59:58 +0100

    Implements Capacitor to work with Apps.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.heartbridge.app",
+  appName: "HeartBridge",
+  webDir: "out",
+};
+
+export default config;
