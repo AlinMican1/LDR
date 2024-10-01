cb39e0d89a662ed1577a0d6eb1054844ae4c5391
AlinMican <AlinMican2001@gmail.com>
2024-10-01 18:45:23 +0100

    starts work on Settings page.

---
+++ components/molecules/settings.tsx
@@ -0,0 +1,10 @@
+import "./settings.css";
+import { LogOutButton } from "../atoms/customButton";
+const Settings = () => {
+  return (
+    <div className="ContentContainer">
+      <LogOutButton />
+    </div>
+  );
+};
+export default Settings;
