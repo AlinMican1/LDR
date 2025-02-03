e58e0f0ff06d9fafaa22b798cf210e95dd7554b6
AlinMican <AlinMican2001@gmail.com>
2025-02-03 17:29:44 +0000

    Works on vibrate feature.

--- components/atoms/vibrateAction.tsx
+++ components/atoms/vibrateAction.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { Haptics, ImpactStyle } from "@capacitor/haptics";
+// import React, { useState } from "react";
+// import { Haptics, ImpactStyle } from "@capacitor/haptics";
 
-const VibrateAction = () => {
-  const [click, setClick] = useState(0);
-  const hapticsVibrate = async () => {
-    await Haptics.vibrate();
-    setClick(click + 1);
-  };
-  return <button onClick={hapticsVibrate}>VibrateAction - {click}</button>;
-};
+// const VibrateAction = () => {
+//   const [click, setClick] = useState(0);
+//   const hapticsVibrate = async () => {
+//     await Haptics.vibrate();
+//     setClick(click + 1);
+//   };
+//   return <button onClick={hapticsVibrate}>VibrateAction - {click}</button>;
+// };
 
-export default VibrateAction;
+// export default VibrateAction;
