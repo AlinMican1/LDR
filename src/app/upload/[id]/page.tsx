7b90e7c9771464ea5071124634aaf72c7ab46152
AlinMican <AlinMican2001@gmail.com>
2025-02-11 18:27:32 +0000

    Works on uploadImage.

--- src/app/upload/[id]/page.tsx
+++ src/app/upload/[id]/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function UploadID() {
-  return <div>SOME PAGE ON YES asdasd</div>;
+  return <div className="ContentContainer">HALLOO LEONEIII !!!!!!!!!!!!!</div>;
 }
