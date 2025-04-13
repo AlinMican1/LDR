4719c2550df9fb12de34ff53ceb972f5c2c2f91e
AlinMican <AlinMican2001@gmail.com>
2025-04-13 15:41:47 +0100

    works on uploadImage through phone.

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { isMobile } from "react-device-detect";
 import UploadImage from "@/components/molecules/uploadImage";
 const Upload = () => {
   return (
     <div>
-      {/* UPLOAD PAGE <img src={QrCode} /> {sessionId} */}
+      
       <UploadImage />
     </div>
   );
